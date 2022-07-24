0950726d6e68c1ffbf0f950b717fe48791009fe7
youssefsaber0 <77590247+youssefsaber0@users.noreply.github.com>
2022-07-24 22:37:57 +0000

    Sync LeetCode submission - Valid Parentheses (java)

---
+++ my-folder/problems/valid_parentheses/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();  
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='['||s.charAt(i)=='{'||s.charAt(i)=='('){
+                stk.push(s.charAt(i));
+            }
+            else if(stk.empty()) return false;
+            else if(s.charAt(i)==']'&&'['==stk.peek()){
+                stk.pop();
+            }
+            else if(s.charAt(i)=='}'&&'{'==stk.peek()){
+                stk.pop();
+            }
+            else if(s.charAt(i)==')'&&'('==stk.peek()){
+                stk.pop();
+            }
+            else return false;
+        }
+        return stk.empty();
+    }
+}
