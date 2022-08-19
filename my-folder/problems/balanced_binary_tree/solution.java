61152c19ecb668dfe0124d60ac01c32ae18a9968
youssefsaber0 <77590247+youssefsaber0@users.noreply.github.com>
2022-08-19 22:18:52 +0000

    Sync LeetCode submission - Balanced Binary Tree (java)

--- my-folder/problems/balanced_binary_tree/solution.java
+++ my-folder/problems/balanced_binary_tree/solution.java
@@ -13,33 +13,29 @@
  *     }
  * }
  */
-import java.util.Hashtable;
-
 class Solution {
-        Hashtable<TreeNode, Integer> ht1 = new Hashtable<>();
+    public int calcHeight(TreeNode node,Map<TreeNode, Integer> heights){
+        if(node == null){
+            return -1;
+        }
+        else if(heights.containsKey(node)){
+            return heights.get(node);
+        }
+        else{
+            int height=Math.max(calcHeight(node.left,heights),calcHeight(node.right,heights))+1;
+            heights.put(node,height);
+            return height;
+        }
+    }
     public boolean isBalanced(TreeNode root) {
+       Map<TreeNode, Integer> heights  = new HashMap<>();
+       return isBalanced(root,heights);
+    }
+     public boolean isBalanced(TreeNode root, Map<TreeNode, Integer> heights){
         if(root==null){
             return true;
         }
-        if(Math.abs(getHeight(root.right)-getHeight(root.left))>1){
-            return false;
-        }
-        
-        return true&&isBalanced(root.right)&&isBalanced(root.left);
-    }
-    public int getHeight(TreeNode root){
-        if(root==null){
-            return -1;
-        }
-        if(ht1.containsKey(root)){
-            return ht1.get(root);
-        }
-        else{
-            int ans =Math.max(getHeight(root.right),getHeight(root.left))+1;
-            ht1.put(root,ans);
-            return ans;
-        }
-    }
-    
-     
+         boolean c=Math.abs(calcHeight(root.left,heights)-calcHeight(root.right,heights))<2;
+         return c&&isBalanced(root.right,heights)&&isBalanced(root.left,heights);
+     }
 }
