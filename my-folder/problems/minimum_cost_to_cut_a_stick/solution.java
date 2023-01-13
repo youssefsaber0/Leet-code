239941e32becca7bf7982c1a7d8be7498ae5e85d
youssefsaber0 <77590247+youssefsaber0@users.noreply.github.com>
2023-01-13 16:15:09 +0000

    Sync LeetCode submission - Minimum Cost to Cut a Stick (java)

--- my-folder/problems/minimum_cost_to_cut_a_stick/solution.java
+++ my-folder/problems/minimum_cost_to_cut_a_stick/solution.java
@@ -1,32 +1,31 @@
 class Solution {
-//     function intialize memorized array
-    public int min_Cost(List<Integer> cuts, int i, int j, int mem[][]){
-        if(j-i <=1){
-            return 0;
-        }
-        else if(mem[i][j]!=Integer.MAX_VALUE){
-            return mem[i][j];
-        }
-        else{
-            for(int k=i+1; k<j; k++){
-                mem[i][j]=Math.min(mem[i][j],min_Cost(cuts,i,k,mem)+min_Cost(cuts,k,j,mem)+cuts.get(j)-cuts.get(i));
-            }
-        }
-        return mem[i][j];
-    }
     public int minCost(int n, int[] cuts) {
-        List<Integer> list=Arrays.stream(cuts)
-      .boxed()
-      .collect(Collectors.toList());
-        list.add(0);
-        list.add(n);
-        Collections.sort(list);
-        int mim[][]=new int[list.size()][list.size()];
-        for (int i=0; i<(mim.length); i++ ) {
-            for (int j=0;j<mim[i].length;j++){
-                mim[i][j]=Integer.MAX_VALUE;
-            }
+        Arrays.sort(cuts);
+        int allCuts[] =new int[cuts.length+2];
+        allCuts[0] = 0;
+        allCuts[allCuts.length - 1] = n;
+        for(int i = 1; i<cuts.length + 1; i++){
+            allCuts[i] = cuts[i - 1] ; 
         }
-        return min_Cost(list, 0, list.size()-1, mim);
+        int dp[][] = new int[allCuts.length][allCuts.length];
+        for(int i = 0; i<allCuts.length; i++){
+            for(int j = 0; j<allCuts.length; j++)
+                dp[i][j] = Integer.MAX_VALUE;
+        }
+        return minCuts(allCuts, 0, allCuts.length - 1, dp);
+    }
+    public int minCuts(int cuts[],int st, int end,int dp[][]){
+        if(dp[st][end] != Integer.MAX_VALUE){
+            return dp[st][end];
+        }
+        if(end-st <= 1 )
+            return 0;
+        for(int i = st +1; i < end ; i++){
+            dp[st][end] = Math.min(
+                dp[st][end],
+                minCuts(cuts, st, i, dp) + minCuts(cuts, i, end, dp) + (cuts[end] - cuts[st])
+            );
+        }
+        return  dp[st][end];  
     }
 }
