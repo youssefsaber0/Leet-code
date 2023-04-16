3d421ba3f5c3ea628980c75d0965b7bc5a534a89
youssefsaber0 <77590247+youssefsaber0@users.noreply.github.com>
2023-04-16 16:31:00 +0000

    Sync LeetCode submission - Nth Highest Salary (mysql)

---
+++ my-folder/problems/nth_highest_salary/solution.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+  RETURN (
+      SELECT(SELECT DISTINCT
+    Salary 
+FROM
+    Employee
+ORDER BY Salary DESC
+LIMIT 1 OFFSET N)AS NHighestSalary
+  );
+END
